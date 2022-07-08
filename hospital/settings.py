0214806c37e3239af141f8c6da5d9b21afe6f307
Wikrom-Th <59578780+Wikrom-Th@users.noreply.github.com>
2022-07-08 15:09:42 +0900

    gitignore potential security risky files

--- hospital/settings.py
+++ hospital/settings.py
@@ -1,3 +1,5 @@
+from secret import SECRET_KEY
+
 """
 Django settings for hospital project.
 
@@ -18,9 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b72o45-9m$&q%yh^b8^cm3sv4knky1-miy@(gt$imr*@_q#t@w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
