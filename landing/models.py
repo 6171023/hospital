546b65510b53bf4d9c4273a8f95c0c95c79cf739
Wikrom-Th <59578780+Wikrom-Th@users.noreply.github.com>
2022-07-08 20:24:58 +0900

    add couple of objects into django model

--- landing/models.py
+++ landing/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
+class Allergy(models.Model):
+    name = models.CharField(max_length=64)
+
 # Create your models here.
+class GeneralUser(models.Model):
+    #TODO: regex validation for dob and tel 
+    name = models.CharField(max_length=256)
+
+    GENDER_CHOICES = [(0, 'Male'),(1, 'Female'),(2, 'Other'), (3, 'Do not wish to answer')]
+    gender = models.IntegerField(choices=GENDER_CHOICES)
+
+    dob = models.DateField()
+    allergies = models.ManyToManyField(Allergy)
+    address = models.TextField(max_length=512)
+    tel = models.CharField(max_length=16)
+    email = models.EmailField(max_length=64)
+
+    def __str__(self):
+        return self.name 
+
+#TODO: Make Doctor class with following values:
+#years_of_experience, qualification, specialization, gender, email
