098a1b8a1e388661a020395a30cb7579663121d5
Wikrom-Th <59578780+Wikrom-Th@users.noreply.github.com>
2022-07-08 16:28:38 +0900

    app for landing page

--- hospital/settings.py
+++ hospital/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'landing.apps.LandingConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
