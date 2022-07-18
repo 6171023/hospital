42c305ae673873a7b17eb8932e1e096b9241a9c4
Wikrom-Th <59578780+Wikrom-Th@users.noreply.github.com>
2022-07-18 11:43:26 +0900

    remove misleading comment

--- landing/models.py
+++ landing/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-#uid will be prefixed with the type of user: 1 is GeneralUser and 2 is Doctor
 
 class GeneralUser(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
